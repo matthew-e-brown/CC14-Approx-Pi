b3b9acaf4fe66e1e12e9e39f461f67224d1d714b
Matthew Brown <matthew.e.brown.17@gmail.com>
2018-11-09 19:52:03 -0500

    Upgraded to use threads

--- approximate.py
+++ approximate.py
@@ -1,16 +1,26 @@
 from random import uniform
+from threading import Thread
 
 totalDots = 0
 circleDots = 0
 radius = 100.0
 
-for i in range(0, int(3.14 * 10 ** 7)): # :P
-    #random.uniform(a, b)
-    x = uniform(-100.0, 100.0)
-    y = uniform(-100.0, 100.0)
-    totalDots += 1
-    if x ** 2 + y ** 2 < radius ** 2: #If it's within the circle
-        circleDots += 1
+def chunk():
+    global totalDots, circleDots, radius
+    for i in range(0, 5 * 10 ** 5): # :P
+        #random.uniform(a, b)
+        x = uniform(-100.0, 100.0)
+        y = uniform(-100.0, 100.0)
+        totalDots += 1
+        if x ** 2 + y ** 2 < radius ** 2: #If it's within the circle
+            circleDots += 1
+
+threads = []
+
+for i in range(0, 15):
+    threads.append( Thread(target = chunk, args = ()) )
+    threads[i].start()
+    threads[i].join()
 
 print("Total Dots: %s\nCircle Dots: %s" % (totalDots, circleDots))
 pi = 4.0 * circleDots / totalDots
